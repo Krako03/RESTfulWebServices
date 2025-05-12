e46d4995eeedcf355caaf7a127e3536922a61f00
Saul Bejar <saulbejar03@gmail.com>
2025-05-12 12:23:31 -0600

    Final part of the proyect

---
+++ src/main/java/com/griddynamics/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,23 @@
+package com.griddynamics.restfulwebservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                authorizeRequests -> authorizeRequests.anyRequest().authenticated()
+                );
+        http.httpBasic(Customizer.withDefaults());
+        http.headers(AbstractHttpConfigurer::disable);
+        http.csrf(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+}
